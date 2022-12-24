ec38014c75413aba0ddaeccbf40e5e187405b53a
Nikita_Shakhtin <nshahtin@mail.ru>
2022-12-24 21:05:53 +0400

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+using static System.Console;
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+ WriteLine(InCenter(number));
+}
+
+int ReadInt(string message)
+{
+ Write(message);
+ return Convert.ToInt32(ReadLine());
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
