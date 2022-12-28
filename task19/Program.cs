322536fe46f3876382c21cf10316641bc5694414
Nikita_Shakhtin <nshahtin@mail.ru>
2022-12-29 02:09:24 +0400

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+string number = ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+ if (number[0] == number[4] && number[1] == number[3])
+    {
+ WriteLine($"{number} - Палиндром");
+    }
+ else
+    {
+ WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,25 @@
+﻿// .Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+using static System.Console;
+Clear();
+
+Write("Точка A1 -> Введите координату x:");
+int.TryParse(ReadLine(), out int ax);
+Write("Точка A2 -> Введите координату y:");
+int.TryParse(ReadLine(), out int ay);
+Write("Точка A3 -> Введите координату z:");
+int.TryParse(ReadLine(), out int az);
+
+Write("Точка B1 -> Введите координату x:");
+int.TryParse(ReadLine(), out int bx);
+Write("Точка B2 -> Введите координату y:");
+int.TryParse(ReadLine(), out int by);
+Write("Точка B3 -> Введите координату z:");
+int.TryParse(ReadLine(), out int bz);
+
+double length = Math.Sqrt(Math.Pow(ax-bx),2)+(Math.Pow(ay-by),2)+(Math.Pow((az-bz),2));
+WriteLine($"{length:f2}");
+
+
+
+    
