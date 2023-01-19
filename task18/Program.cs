b9f2f206a013e8775b09b88e5f61e5588fb2701b
Nikita_Shakhtin <nshahtin@mail.ru>
2023-01-19 18:55:46 +0400

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+switch (i)
+{
+case 1:
+{
+WriteLine("x>0 y>0");
+break;
+}
+case 2:
+{
+WriteLine("x<0 y>0");
+break;
+}
+case 3:
+{
+WriteLine("x<0 y<0");
+break;
+}
+case 4:
+{
+WriteLine("x>0 y<0");
+break;
+}
+default:
+{
+WriteLine("Ошибка");
+break;
+}
+}
+
+
+Write("Введите номер четверти:");
+bool isParse=int.TryParse(ReadLine(),out int i);
+if(!isParse)
+{
+WriteLine("Введено не число");
+return;
+}
